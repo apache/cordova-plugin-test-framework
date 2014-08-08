1aea198223f83535214b2c024f1e35d44cb9fc05
Jesse MacFadyen <purplecabbage@gmail.com>
2014-08-07 17:29:48 -0700

    Fix indentation to 4 spaces from 2

--- www/jasmine_helpers.js
+++ www/jasmine_helpers.js
@@ -22,72 +22,72 @@
 'use strict';
 
 exports.setUpJasmine = function() {
-  // Set up jasmine
-  var jasmine = jasmineRequire.core(jasmineRequire);
-  jasmineRequire.html(jasmine);
-  var jasmineEnv = jasmine.currentEnv_ = new jasmine.Env();
+    // Set up jasmine
+    var jasmine = jasmineRequire.core(jasmineRequire);
+    jasmineRequire.html(jasmine);
+    var jasmineEnv = jasmine.currentEnv_ = new jasmine.Env();
 
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
-  jasmineEnv.catchExceptions(false);
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+    jasmineEnv.catchExceptions(false);
 
-  // Set up jasmine interface
-  var jasmineInterface = Object.create(null);
-  jasmineInterface.jasmine = jasmine;
+    // Set up jasmine interface
+    var jasmineInterface = Object.create(null);
+    jasmineInterface.jasmine = jasmine;
 
-  // Fill in jasmineInterface with built-ins
-  var jasmine_env_functions = ['describe',
-                               'xdescribe',
-                               'it',
-                               'xit',
-                               'beforeEach',
-                               'afterEach',
-                               'expect',
-                               'pending',
-                               'spyOn',
-                               'addCustomEqualityTester',
-                               'addMatchers'];
+    // Fill in jasmineInterface with built-ins
+    var jasmine_env_functions = ['describe',
+                                 'xdescribe',
+                                 'it',
+                                 'xit',
+                                 'beforeEach',
+                                 'afterEach',
+                                 'expect',
+                                 'pending',
+                                 'spyOn',
+                                 'addCustomEqualityTester',
+                                 'addMatchers'];
 
-  jasmine_env_functions.forEach(function(fn) {
+    jasmine_env_functions.forEach(function(fn) {
     jasmineInterface[fn] = jasmineEnv[fn].bind(jasmineEnv);
-  });
-  jasmineInterface.clock = jasmineEnv.clock;
+    });
+    jasmineInterface.clock = jasmineEnv.clock;
 
-  // Add Reporters
-  addJasmineReporters(jasmineInterface, jasmineEnv);
+    // Add Reporters
+    addJasmineReporters(jasmineInterface, jasmineEnv);
 
-  // Add Spec Filter
-  jasmineEnv.specFilter = function(spec) {
+    // Add Spec Filter
+    jasmineEnv.specFilter = function(spec) {
     //console.log(spec.getFullName());
     return true;
-  };
+    };
 
-  return jasmineInterface;
+    return jasmineInterface;
 }
 
 function addJasmineReporters(jasmineInterface, jasmineEnv) {
-  jasmineInterface.jsApiReporter = new jasmineInterface.jasmine.JsApiReporter({ timer: new jasmineInterface.jasmine.Timer() });
-  jasmineEnv.addReporter(jasmineInterface.jsApiReporter);
+    jasmineInterface.jsApiReporter = new jasmineInterface.jasmine.JsApiReporter({ timer: new jasmineInterface.jasmine.Timer() });
+    jasmineEnv.addReporter(jasmineInterface.jsApiReporter);
 
-  jasmineInterface.htmlReporter = new jasmineInterface.jasmine.HtmlReporter({
-    env: jasmineEnv,
-    queryString: function() { return null; },
-    onRaiseExceptionsClick: function() { },
-    getContainer: function() { return document.getElementById('content'); },
-    createElement: function() { return document.createElement.apply(document, arguments); },
-    createTextNode: function() { return document.createTextNode.apply(document, arguments); },
-    timer: new jasmineInterface.jasmine.Timer()
-  });
-  jasmineInterface.htmlReporter.initialize();
-  jasmineEnv.addReporter(jasmineInterface.htmlReporter);
-
-  var medic = require('org.apache.cordova.test-framework.medic');
-  if (medic.enabled) {
-    jasmineRequire.medic(jasmineInterface.jasmine);
-    jasmineInterface.MedicReporter = new jasmineInterface.jasmine.MedicReporter({
-      env: jasmineEnv,
-      log: { logurl: medic.logurl }
+    jasmineInterface.htmlReporter = new jasmineInterface.jasmine.HtmlReporter({
+        env: jasmineEnv,
+        queryString: function() { return null; },
+        onRaiseExceptionsClick: function() { },
+        getContainer: function() { return document.getElementById('content'); },
+        createElement: function() { return document.createElement.apply(document, arguments); },
+        createTextNode: function() { return document.createTextNode.apply(document, arguments); },
+        timer: new jasmineInterface.jasmine.Timer()
     });
-    jasmineInterface.MedicReporter.initialize();
-    jasmineEnv.addReporter(jasmineInterface.MedicReporter);
-  }
+    jasmineInterface.htmlReporter.initialize();
+    jasmineEnv.addReporter(jasmineInterface.htmlReporter);
+
+    var medic = require('org.apache.cordova.test-framework.medic');
+    if (medic.enabled) {
+        jasmineRequire.medic(jasmineInterface.jasmine);
+        jasmineInterface.MedicReporter = new jasmineInterface.jasmine.MedicReporter({
+            env: jasmineEnv,
+            log: { logurl: medic.logurl }
+        });
+        jasmineInterface.MedicReporter.initialize();
+        jasmineEnv.addReporter(jasmineInterface.MedicReporter);
+    }
 }
